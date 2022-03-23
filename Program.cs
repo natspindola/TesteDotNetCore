119120e491a4011ca93a8d0bac3ce25ae039c4a7
natspindola <natspindola@hotmail.com>
2022-03-23 17:52:51 -0300

    teste

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace TesteDotNetCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Digite sua idade: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Olá, {nome}");
+            Console.WriteLine($"Você tem {idade} anos");
         }
     }
 }

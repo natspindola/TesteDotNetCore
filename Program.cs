d0a45ae91c37d0b0f5310ab511910c7ce918ba1b
natspindola <natspindola@hotmail.com>
2022-03-23 17:47:56 -0300

    ASP.NET Core

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
